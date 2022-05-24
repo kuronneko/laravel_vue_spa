d4cdb417caf6bb44beab11174860b41495be9cde
maindesktop <maindesktop@maindesktop.com>
2022-05-24 04:33:10 -0400

    part 1

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Laravel + Vue SPA</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <link href="{{ mix('css/app.css')}}" type="text/css" rel="stylesheet" >
+</head>
+<body>
+    <div id="app">
+
+    </div>
+    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
